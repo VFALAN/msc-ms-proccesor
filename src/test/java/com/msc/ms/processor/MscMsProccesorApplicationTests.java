77553b9d766ac916ca271ad2a9530235bb7329c5
VFALAN <alanvillafan95@gmail.com>
2024-05-06 13:51:23 -0600

    first commit

---
+++ src/test/java/com/msc/ms/processor/MscMsProccesorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.ms.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscMsProccesorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
